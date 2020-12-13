beecff09c7c4cfc4e74c93a18977cfb7ca1cb518
Bernhard Kralofsky <bernhard@kralofsky.com>
2020-12-13 11:33:47 +0100

    day 13

---
+++ 2020/13.py
@@ -0,0 +1,35 @@
+import itertools
+import math
+
+from aocd import get_data
+
+
+def part1(a):
+    first = next(
+        itertools.chain.from_iterable(((t, l) for l in a[1] if t % l == 0) for t in range(a[0], a[0] + min(a[1]))))
+    return (first[0] - a[0]) * first[1]
+
+
+def lcm(x, y):
+    return x * y // math.gcd(x, y)
+
+
+def part2(a):
+    a = [(i, int(n)) for i, n in enumerate(a[2]) if n != 'x']
+    number = 0
+    current_lcm = 1
+    for i, n in a:
+        for j in range(1, n+1):
+            if (number + current_lcm*j + i) % n == 0:
+                number += current_lcm*j
+                break
+        current_lcm = lcm(current_lcm, n)
+    return number
+
+
+if __name__ == '__main__':
+    data = get_data(day=13, year=2020)
+    input = data.splitlines()
+    input = (int(input[0]), [int(i) for i in input[1].split(',') if i != 'x'], input[1].split(','))
+    print(part1(input))
+    print(part2(input))
