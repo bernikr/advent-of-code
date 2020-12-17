d21e07d536202410a2d45f7d7acc23f447ef8cb3
Bernhard Kralofsky <bernhard@kralofsky.com>
2020-12-17 21:26:54 +0100

    refactor day 17

--- 2020/17.py
+++ 2020/17.py
@@ -1,51 +1,59 @@
 import itertools
+import operator
 
 from aocd import get_data
 
 
-def neighbors3d(x, y, z):
-    return ((x+i, y+j, z+k) for i, j, k in itertools.product([-1, 0, 1], repeat=3) if not (i == 0 and j == 0 and k == 0))
+def simulate(start_set, neighbor_function, rule_function, rounds):
+    active = start_set
+    for i in range(rounds):
+        next_active = set()
+        empty_neighbors = set()
+        for cell in active:
+            neighbors = neighbor_function(cell)
+            empty_neighbors.update(c for c in neighbors if c not in active)
+            num = sum(c in active for c in neighbors)
+            if rule_function(num, True):
+                next_active.add(cell)
+        for cell in empty_neighbors:
+            num = sum(c in active for c in neighbor_function(cell))
+            if rule_function(num, False):
+                next_active.add(cell)
+        active = next_active
+    return active
+
+
+def rules(num, active):
+    if active:
+        return 2 <= num <= 3
+    else:
+        return num == 3
+
+
+def neighbors3d(c):
+    return [tuple(map(operator.add, c, n)) for n in itertools.product([-1, 0, 1], repeat=3) if not n == (0, 0, 0)]
 
 
 def part1(a):
-    active = set()
+    start = set()
     for x in range(len(a)):
         for y in range(len(a[x])):
             if a[x][y] == '#':
-                active.add((x, y, 0))
-    for i in range(6):
-        next = set()
-        for x in range(min(c[0] for c in active)-1, max(c[0] for c in active)+2):
-            for y in range(min(c[1] for c in active)-1, max(c[1] for c in active)+2):
-                for z in range(min(c[2] for c in active)-1, max(c[2] for c in active)+2):
-                    num = sum((a, b, c) in active for a, b, c in neighbors3d(x, y, z))
-                    if ((x, y, z) in active and 2 <= num <= 3) or ((x, y, z) not in active and num == 3):
-                        next.add((x, y, z))
-        active = next
-    return len(active)
+                start.add((x, y, 0))
+    return len(simulate(start, neighbors3d, rules, 6))
 
 
-def neighbors4d(x, y, z, w):
-    return ((x+i, y+j, z+k, w+l) for i, j, k, l in itertools.product([-1, 0, 1], repeat=4) if not (i == 0 and j == 0 and k == 0 and l == 0))
+def neighbors4d(c):
+    return [tuple(map(operator.add, c, n)) for n in itertools.product([-1, 0, 1], repeat=4) if not n == (0, 0, 0, 0)]
 
 
 def part2(a):
-    active = set()
+    start = set()
     for x in range(len(a)):
         for y in range(len(a[x])):
             if a[x][y] == '#':
-                active.add((x, y, 0, 0))
-    for i in range(6):
-        next = set()
-        for x in range(min(c[0] for c in active)-1, max(c[0] for c in active)+2):
-            for y in range(min(c[1] for c in active)-1, max(c[1] for c in active)+2):
-                for z in range(min(c[2] for c in active)-1, max(c[2] for c in active)+2):
-                    for w in range(min(c[3] for c in active)-1, max(c[3] for c in active)+2):
-                        num = sum((a, b, c, d) in active for a, b, c, d in neighbors4d(x, y, z, w))
-                        if ((x, y, z, w) in active and 2 <= num <= 3) or ((x, y, z, w) not in active and num == 3):
-                            next.add((x, y, z, w))
-        active = next
-    return len(active)
+                start.add((x, y, 0, 0))
+    return len(simulate(start, neighbors4d, rules, 6))
 
 
 if __name__ == '__main__':
