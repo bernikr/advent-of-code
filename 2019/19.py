0f822711919021621653d62e2855f181b150606f
bernikr <berni98k@gmail.com>
2021-12-01 22:43:45 -0100

    2019 day 19 part 1

---
+++ 2019/19.py
@@ -0,0 +1,96 @@
+from collections import defaultdict
+from itertools import chain
+
+from aocd import get_data
+
+
+def get_parameter_address(p, ip, base, n):
+    flags = p[ip] // 100
+    flag = (flags // 10**(n-1)) % 10
+    match flag:
+        case 0:
+            return p[ip+n]
+        case 1:
+            assert False, "Invalid use of immediate flag"
+        case 2:
+            return p[ip+n]+base
+        case i:
+            assert False, f"Unknown Parameter Flag {i}"
+
+
+def get_parameter(p, ip, base, n):
+    flags = p[ip] // 100
+    flag = (flags // 10**(n-1)) % 10
+    match flag:
+        case 0 | 2:
+            return p[get_parameter_address(p, ip, base, n)]
+        case 1:
+            return p[ip+n]
+        case i:
+            assert False, f"Unknown Parameter Flag {i}"
+
+
+def execute(p, inp):
+    inp = chain(inp)
+    p = defaultdict(lambda: 0, {k: v for k, v in enumerate(p)})
+    ip = 0
+    base = 0
+    while True:
+        match p[ip] % 100:
+            case 1:
+                p[get_parameter_address(p, ip, base, 3)] = get_parameter(p, ip, base, 1) + get_parameter(p, ip, base, 2)
+                ip += 4
+            case 2:
+                p[get_parameter_address(p, ip, base, 3)] = get_parameter(p, ip, base, 1) * get_parameter(p, ip, base, 2)
+                ip += 4
+            case 3:
+                p[get_parameter_address(p, ip, base, 1)] = next(inp)
+                ip += 2
+            case 4:
+                yield get_parameter(p, ip, base, 1)
+                ip += 2
+            case 5:
+                if get_parameter(p, ip, base, 1) != 0:
+                    ip = get_parameter(p, ip, base, 2)
+                else:
+                    ip += 3
+            case 6:
+                if get_parameter(p, ip, base, 1) == 0:
+                    ip = get_parameter(p, ip, base, 2)
+                else:
+                    ip += 3
+            case 7:
+                p[get_parameter_address(p, ip, base, 3)] = 1 if get_parameter(p, ip, base, 1) < get_parameter(p, ip, base, 2) else 0
+                ip += 4
+            case 8:
+                p[get_parameter_address(p, ip, base, 3)] = 1 if get_parameter(p, ip, base, 1) == get_parameter(p, ip, base, 2) else 0
+                ip += 4
+            case 9:
+                base += get_parameter(p, ip, base, 1)
+                ip += 2
+            case 99:
+                break
+            case i:
+                assert False, f"Instruction {i} not implemented yet"
+
+
+def part1(a):
+    p = a.copy()
+    out = ""
+    for y in range(50):
+        for x in range(50):
+            out += '#' if next(execute(p, [x, y])) else '.'
+        out += '\n'
+    return sum(c == '#' for c in out)
+
+
+def part2(a):
+    p = a.copy()
+    return None
+
+
+if __name__ == '__main__':
+    data = get_data(day=19, year=2019)
+    inp = list(map(int, data.split(',')))
+    print(part1(inp))
+    print(part2(inp))
