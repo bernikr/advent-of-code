a7f4f84b6ebcd1ce03f5c065b897d963b1e78ee5
bernikr <berni98k@gmail.com>
2021-12-01 15:17:49 -0100

    add template

---
+++ _.py
@@ -0,0 +1,16 @@
+from aocd import get_data
+
+
+def part1(inp):
+    return inp
+
+
+def part2(inp):
+    return None
+
+
+if __name__ == '__main__':
+    data = get_data(day=0, year=2021)
+    inp = data.splitlines()
+    print(part1(inp))
+    print(part2(inp))
