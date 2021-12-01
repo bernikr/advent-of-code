07fb1681ba94a0f993ade0a35fe946500e2d4f7e
bernikr <berni98k@gmail.com>
2021-12-01 22:30:09 -0100

    2019 day 17 part 2

--- 2019/17.py
+++ 2019/17.py
@@ -1,5 +1,5 @@
 import operator
-from collections import defaultdict
+from collections import defaultdict, Counter
 from enum import Enum
 from itertools import chain
 
@@ -8,26 +8,26 @@ from aocd import get_data
 
 def get_parameter_address(p, ip, base, n):
     flags = p[ip] // 100
-    flag = (flags // 10**(n-1)) % 10
+    flag = (flags // 10 ** (n - 1)) % 10
     match flag:
         case 0:
-            return p[ip+n]
+            return p[ip + n]
         case 1:
             assert False, "Invalid use of immediate flag"
         case 2:
-            return p[ip+n]+base
+            return p[ip + n] + base
         case i:
             assert False, f"Unknown Parameter Flag {i}"
 
 
 def get_parameter(p, ip, base, n):
     flags = p[ip] // 100
-    flag = (flags // 10**(n-1)) % 10
+    flag = (flags // 10 ** (n - 1)) % 10
     match flag:
         case 0 | 2:
             return p[get_parameter_address(p, ip, base, n)]
         case 1:
-            return p[ip+n]
+            return p[ip + n]
         case i:
             assert False, f"Unknown Parameter Flag {i}"
 
@@ -62,10 +62,14 @@ def execute(p, inp):
                 else:
                     ip += 3
             case 7:
-                p[get_parameter_address(p, ip, base, 3)] = 1 if get_parameter(p, ip, base, 1) < get_parameter(p, ip, base, 2) else 0
+                p[get_parameter_address(p, ip, base, 3)] = 1 if get_parameter(p, ip, base, 1) < get_parameter(p, ip,
+                                                                                                              base,
+                                                                                                              2) else 0
                 ip += 4
             case 8:
-                p[get_parameter_address(p, ip, base, 3)] = 1 if get_parameter(p, ip, base, 1) == get_parameter(p, ip, base, 2) else 0
+                p[get_parameter_address(p, ip, base, 3)] = 1 if get_parameter(p, ip, base, 1) == get_parameter(p, ip,
+                                                                                                               base,
+                                                                                                               2) else 0
                 ip += 4
             case 9:
                 base += get_parameter(p, ip, base, 1)
@@ -96,16 +100,27 @@ class Dir(Enum):
     def move(self, c):
         return tuple(map(operator.add, c, self.to_coords()))
 
-    def opposite(self):
+    def turn_left(self):
         match self:
             case self.NORTH:
-                return self.SOUTH
-            case self.SOUTH:
-                return self.NORTH
-            case self.EAST:
                 return self.WEST
             case self.WEST:
+                return self.SOUTH
+            case self.SOUTH:
                 return self.EAST
+            case self.EAST:
+                return self.NORTH
+
+    def turn_right(self):
+        match self:
+            case self.NORTH:
+                return self.EAST
+            case self.EAST:
+                return self.SOUTH
+            case self.SOUTH:
+                return self.WEST
+            case self.WEST:
+                return self.NORTH
 
 
 def part1(a):
@@ -113,12 +128,69 @@ def part1(a):
     mapp = defaultdict(lambda: '.',
                        {(x, y): c for y, l in enumerate(''.join(map(chr, execute(p, []))).splitlines())
                         for x, c in enumerate(l)})
-    return sum(x*y for x, y in mapp.copy() if mapp[(x, y)] == '#' and all(mapp[d.move((x, y))] == '#' for d in Dir))
+    return sum(x * y for x, y in mapp.copy() if mapp[(x, y)] == '#' and all(mapp[d.move((x, y))] == '#' for d in Dir))
+
+
+# https://stackoverflow.com/a/11091393
+def find_repeated_substring(a, times):
+    for n in range(1, 20)[::-1]:
+        substrings = [a[i:i + n] for i in range(len(a) - n + 1)]
+        freqs = Counter(substrings)
+        if freqs.most_common(1)[0][1] >= times:
+            return freqs.most_common(1)[0][0]
 
 
 def part2(a):
     p = a.copy()
-    return None
+    mapp = ''.join(map(chr, execute(p, [])))
+    print(mapp)
+    path = ["L"]  # start direction needs to be manually added to path and d
+    d = Dir.WEST
+    mov = 0
+    mapp = defaultdict(lambda: '.',
+                       {(x, y): c for y, l in enumerate(''.join(map(chr, execute(p, []))).splitlines())
+                        for x, c in enumerate(l)})
+    pos = next(c for c in mapp if mapp[c] in "<>^v")
+    while True:
+        if mapp[d.move(pos)] == '#':
+            pos = d.move(pos)
+            mov += 1
+        else:
+            path.append(str(mov))
+            mov = 0
+            if mapp[d.turn_left().move(pos)] == "#":
+                d = d.turn_left()
+                path.append("L")
+            elif mapp[d.turn_right().move(pos)] == "#":
+                d = d.turn_right()
+                path.append("R")
+            else:
+                break
+    path = ','.join(path)
+    # integers in the repeated_substring method must be tweaked manually
+    print(f"PATH: {path}")
+    path = ',' + path + ','
+    A = find_repeated_substring(path, 3)[1:-1]
+    print(f"A: {A}")
+    assert len(A) <= 20
+    path = path.replace(A, "A")
+    B = find_repeated_substring(path, 3)[1:-1]
+    print(f"B: {B}")
+    assert len(B) <= 20
+    assert not any('A' == c for c in B)
+    path = path.replace(B, "B")
+    C = find_repeated_substring(path, 3)[1:-1]
+    print(f"C: {C}")
+    assert len(C) <= 20
+    assert not any(c in ['A', 'B'] for c in B)
+    path = path.replace(C, "C")
+    path = path[1:-1]
+    print(f"PATH: {path}")
+    assert len(path) <= 20
+    payload = f"{path}\n{A}\n{B}\n{C}\nn\n"
+    p = a.copy()
+    p[0] = 2
+    return list(execute(p, map(ord, payload)))[-1]
 
 
 if __name__ == '__main__':
