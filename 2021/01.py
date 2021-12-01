5750ec6c345f5129233487e3d14c676e22c1558e
bernikr <berni98k@gmail.com>
2021-12-01 15:05:35 -0100

    2021 day 1

---
+++ 2021/01.py
@@ -0,0 +1,18 @@
+from itertools import pairwise
+
+from aocd import get_data
+
+
+def part1(inp):
+    return sum(1 if b > a else 0 for a, b in pairwise(inp))
+
+
+def part2(inp):
+    return sum(1 if b > a else 0 for a, b in zip(inp, inp[3:]))
+
+
+if __name__ == '__main__':
+    data = get_data(day=1, year=2021)
+    inp = list(map(int, data.splitlines()))
+    print(part1(inp))
+    print(part2(inp))
