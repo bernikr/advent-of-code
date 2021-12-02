f724c23cdbd013468754f0c32e3a62eea05f0adc
bernikr <berni98k@gmail.com>
2021-12-01 23:28:07 -0100

    2019 day 16 part 2

--- 2019/16.py
+++ 2019/16.py
@@ -1,8 +1,7 @@
-import math
-import operator
-from functools import reduce, cache
-from itertools import chain, repeat, cycle, takewhile, product
+from functools import reduce
+from itertools import chain, repeat, cycle
 
+from tqdm import tqdm
 from aocd import get_data
 
 base_pattern = [0, 1, 0, -1]
@@ -25,7 +24,18 @@ def part1(inp):
 
 
 def part2(inp):
-    return None
+    start = int(''.join(map(str, inp[:7])))
+    a = inp * 10000
+    assert start >= len(a)/2, "This method only works for special cases"
+    a = a[start:]
+    for _ in tqdm(range(100)):
+        out = []
+        s = sum(a)
+        for n in a:
+            out.append(s % 10)
+            s -= n
+        a = out
+    return ''.join(map(str, a[:8]))
 
 
 if __name__ == '__main__':
