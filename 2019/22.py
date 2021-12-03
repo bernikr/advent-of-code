e1a4eb9c577ca132b4b1d2b90a0eb1e854cec45e
bernikr <berni98k@gmail.com>
2021-12-03 21:47:27 -0100

    2019 day 22 part 2

--- 2019/22.py
+++ 2019/22.py
@@ -1,3 +1,6 @@
+import math
+from itertools import chain
+
 from aocd import get_data
 
 
@@ -37,8 +40,59 @@ def part1(inp):
     return deck.index(2019)
 
 
+def simplify(ops, N=10007):
+    nops = []
+    for op, n in ops:
+        if op != "new":
+            nops.append((op, n))
+        else:
+            nops.append(("inc", N - 1))
+            nops.append(("cut", 1))
+    ops = nops
+    while len(ops) > 2:
+        nops = []
+        i = 0
+        while i < len(ops):
+            if i == len(ops) - 1:
+                nops.append(ops[i])
+                i += 1
+            else:
+                a, an = ops[i]
+                b, bn = ops[i + 1]
+                if a == b == "cut":
+                    nops.append(("cut", (an + bn) % N))
+                    i += 2
+                elif a == b == "inc":
+                    nops.append(("inc", (an * bn) % N))
+                    i += 2
+                elif a == "cut" and b == "inc":
+                    nops.append(("inc", bn))
+                    nops.append(("cut", (an * bn) % N))
+                    i += 2
+                else:
+                    nops.append((a, an))
+                    i += 1
+        ops = nops
+    return ops
+
+
+def create_multi_shuffle(ops, n, N=10007):
+    n_bin = format(n, 'b')
+    ops = simplify(ops, N=N)
+    shuffle_powers = [[] for _ in n_bin]
+    shuffle_powers[0] = ops
+    for i in range(1, len(shuffle_powers)):
+        shuffle_powers[i] = simplify(shuffle_powers[i - 1] * 2, N=N)
+    return simplify(chain.from_iterable(shuffle_powers[i] for i, b in enumerate(reversed(n_bin)) if b == '1'), N=N)
+
+
 def part2(inp):
-    return None
+    shuffles = 101741582076661
+    deck_size = 119315717514047
+    ops = inp.copy()
+    ops = create_multi_shuffle(ops, shuffles, deck_size)
+    assert len(ops) == 2
+    return f"Wolfram Alpha this: `(x * {ops[0][1]}) - {ops[1][1]} = 2020 mod {deck_size}`"
 
 
 if __name__ == '__main__':
