c54c6d121c0ba411104fb090a4f22f82551d8296
bernikr <berni98k@gmail.com>
2021-12-02 23:56:56 -0100

    2019 day 24 part 2

--- 2019/24.py
+++ 2019/24.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 from aocd import get_data
 
 
@@ -6,7 +8,7 @@ def get_neighbors(x, y):
 
 
 def count_neighbors(mapp, x, y):
-    return sum(mapp[c[1]*5+c[0]] == "#" for c in get_neighbors(x, y))
+    return sum(mapp[c[1] * 5 + c[0]] == "#" for c in get_neighbors(x, y))
 
 
 def part1(inp):
@@ -24,12 +26,43 @@ def part1(inp):
                 nf += f
         field = nf
         if field in layouts:
-            return sum(2**n for n, c in enumerate(field) if c == '#')
+            return sum(2 ** n for n, c in enumerate(field) if c == '#')
         layouts.append(field)
 
 
+def get_recursive_neighbors(x, y, d):
+    neighbors = list(filter(lambda c: 0 <= c[0] < 5 and 0 <= c[1] < 5 and not c[:2] == (2, 2),
+                            [(x - 1, y, d), (x + 1, y, d), (x, y - 1, d), (x, y + 1, d)]))
+    if x == 0:
+        neighbors.append((1, 2, d - 1))
+    if x == 4:
+        neighbors.append((3, 2, d - 1))
+    if y == 0:
+        neighbors.append((2, 1, d - 1))
+    if y == 4:
+        neighbors.append((2, 3, d - 1))
+    if (x, y) == (2, 1):
+        neighbors += [(i, 0, d + 1) for i in range(5)]
+    if (x, y) == (2, 3):
+        neighbors += [(i, 4, d + 1) for i in range(5)]
+    if (x, y) == (1, 2):
+        neighbors += [(0, i, d + 1) for i in range(5)]
+    if (x, y) == (3, 2):
+        neighbors += [(4, i, d + 1) for i in range(5)]
+    return neighbors
+
+
 def part2(inp):
-    return None
+    bugs = {(i % 5, i // 5, 0) for i, c in enumerate(inp) if c == '#'}
+    for _ in range(200):
+        poi = set(chain(chain.from_iterable(get_recursive_neighbors(*b) for b in bugs), bugs))
+        nb = set()
+        for p in poi:
+            n = len(bugs.intersection(get_recursive_neighbors(*p)))
+            if (p in bugs and n == 1) or (p not in bugs and n in [1, 2]):
+                nb.add(p)
+        bugs = nb
+    return len(bugs)
 
 
 if __name__ == '__main__':
