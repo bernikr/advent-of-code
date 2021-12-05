b10dc6a245eabdb97f1ecfad76f3073cd80ca402
bernikr <berni98k@gmail.com>
2021-12-05 17:38:58 -0100

    2019 day 18 optimize part 1

--- 2019/18.py
+++ 2019/18.py
@@ -1,8 +1,10 @@
 import operator
+from collections import defaultdict
 from enum import Enum
 from functools import cache
 
 from aocd import get_data
+from frozendict import frozendict
 
 
 class Dir(Enum):
@@ -15,50 +17,59 @@ class Dir(Enum):
         return tuple(map(operator.add, c, self.value))
 
 
-@cache
-def find(letter):
+def find(mapp, letter):
     cs = [c for c, v in mapp.items() if v == letter]
     assert len(cs) == 1
     return cs[0]
 
 
 @cache
-def steps_to_available_keys(start_pos, current_keys):
-    boundary = {find(start_pos)}
-    visited = boundary.copy()
-    res = {}
+def steps_and_requirements_to_keys(mapp, start_pos):
+    boundary = {(find(mapp, start_pos), frozenset())}
+    visited = defaultdict(lambda: [], {find(mapp, start_pos): [frozenset()]})
+    res = defaultdict(lambda: [])
     steps = 1
     while boundary:
         nb = set()
-        for b in boundary:
+        for b, keys in boundary:
             for d in Dir:
                 nc = d.move(b)
-                if nc in visited:
+                if nc in visited and any(reqs.issubset(keys) for reqs in visited[nc]):
                     continue
                 match mapp[nc]:
                     case "." | "@":
-                        nb.add(nc)
-                        visited.add(nc)
-                    case x if x.lower() in current_keys:
-                        nb.add(nc)
-                        visited.add(nc)
-                    case k if k.islower() and k not in current_keys:
-                        res[k] = steps
+                        nb.add((nc, keys))
+                        visited[nc].append(keys)
+                    case k if k.islower():
+                        nb.add((nc, keys))
+                        visited[nc].append(keys)
+                        res[k].append((keys, steps))
+                    case door if door.isupper():
+                        nb.add((nc, keys.union({door.lower()})))
+                        visited[nc].append(keys.union({door.lower()}))
         boundary = nb
         steps += 1
     return res
 
 
 @cache
-def min_steps(pos, keys):
-    key_distances = steps_to_available_keys(pos, keys)
+def steps_to_available_keys(mapp, start_pos, current_keys):
+    return {k: min(d for reqs, d in req_dists if reqs.issubset(current_keys))
+            for k, req_dists in steps_and_requirements_to_keys(mapp, start_pos).items()
+            if any(reqs.issubset(current_keys) for reqs, d in req_dists) and k not in current_keys}
+
+
+@cache
+def min_steps(mapp, pos, keys):
+    key_distances = steps_to_available_keys(mapp, pos, keys)
     if not key_distances:
         return 0
-    return min(d + min_steps(k, keys.union({k})) for k, d in key_distances.items())
+    return min(d + min_steps(mapp, k, keys.union({k})) for k, d in key_distances.items())
 
 
-def part1():
-    return min_steps('@', frozenset())
+def part1(inp):
+    return min_steps(frozendict(inp), '@', frozenset())
+
 
 
 def part2():
@@ -67,6 +78,6 @@ def part2():
 
 if __name__ == '__main__':
     data = get_data(day=18, year=2019)
-    mapp: dict[tuple[int, int], str] = {(x, y): c for y, l in enumerate(data.splitlines()) for x, c in enumerate(l)}
-    print(part1())
-    print(part2())
+    inp = {(x, y): c for y, l in enumerate(data.splitlines()) for x, c in enumerate(l)}
+    print(part1(inp))
+    print(part2(inp))
