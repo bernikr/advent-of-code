a8a7aafc56271740ae6fb42cce3ec1f7aac47abf
bernikr <berni98k@gmail.com>
2021-12-16 22:49:46 +0100

    2017 day 17 part 2

--- 2017/17.py
+++ 2017/17.py
@@ -1,4 +1,5 @@
 from aocd import get_data
+from tqdm import tqdm
 
 
 def part1(inp):
@@ -11,7 +12,15 @@ def part1(inp):
 
 
 def part2(inp):
-    return None
+    pos = 0
+    last = 0
+    length = 1
+    for i in tqdm(range(50_000_000)):
+        pos = (pos + inp) % length + 1
+        length += 1
+        if pos == 1:
+            last = i + 1
+    return last
 
 
 if __name__ == '__main__':
