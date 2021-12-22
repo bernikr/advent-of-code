331e5257b9f812776d0723c7cdec349770498d34
bernikr <berni98k@gmail.com>
2021-12-22 01:04:53 +0100

    2021 day 20

---
+++ 2021/20.py
@@ -0,0 +1,50 @@
+import math
+from collections import defaultdict
+
+from aoc_utils import Vec
+from aocd import get_data
+
+square = [
+    Vec(-1, -1), Vec(0, -1), Vec(1, -1),
+    Vec(-1, 0), Vec(0, 0), Vec(1, 0),
+    Vec(-1, 1), Vec(0, 1), Vec(1, 1),
+]
+
+
+def enhance(img, alg):
+    xmin = min(x for x, _ in img) - 1
+    xmax = max(x for x, _ in img) + 1
+    ymin = min(y for _, y in img) - 1
+    ymax = max(y for _, y in img) + 1
+    if img.default_factory():
+        default = alg[-1]
+    else:
+        default = alg[0]
+    return defaultdict(lambda: default,
+                       {Vec(x, y): alg[int(''.join('1' if img[(Vec(x, y) + c)] else '0' for c in square), 2)]
+                        for x in range(xmin, xmax + 1) for y in range(ymin, ymax + 1)})
+
+
+def part1(inp):
+    alg, img = inp
+    for _ in range(2):
+        img = enhance(img, alg)
+    return sum(img.values()) if not img.default_factory() else math.inf
+
+
+def part2(inp):
+    alg, img = inp
+    for _ in range(50):
+        img = enhance(img, alg)
+    return sum(img.values()) if not img.default_factory() else math.inf
+
+
+if __name__ == '__main__':
+    data = get_data(day=20, year=2021)
+    inp1, inp2 = data.split('\n\n')
+    inp1 = [c == '#' for c in inp1.replace('\n', '')]
+    inp2 = defaultdict(lambda: False,
+                       {Vec(x, y): c == '#' for y, l in enumerate(inp2.splitlines()) for x, c in enumerate(l)})
+    inp = (inp1, inp2)
+    print(part1(inp))
+    print(part2(inp))
