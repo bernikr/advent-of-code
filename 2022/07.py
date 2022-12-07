31d743c66463470ee7d3c05fa296217840a37238
bernikr <berni98k@gmail.com>
2022-12-07 10:03:00 +0100

    2022 07

---
+++ 2022/07.py
@@ -0,0 +1,48 @@
+from aocd import data, submit, AocdError
+
+
+def get_files_and_dirs(inp):
+    dir = ""
+    files = {}
+    dirs = ['/']
+    for c in inp:
+        match c:
+            case []:
+                pass
+            case [('cd', '/')]:
+                dir = '/'
+            case [('cd', '..')]:
+                dir = '/'.join(dir.split('/')[:-2]) + '/'
+            case [('cd', d)]:
+                dir += d + '/'
+            case [('ls', ), *fs]:
+                for size, filename in fs:
+                    if size == "dir":
+                        dirs.append(dir + filename + '/')
+                    else:
+                        files[dir + filename] = int(size)
+    return files, dirs
+
+
+def part1(inp):
+    files, dirs = inp
+    return sum(s for s in (sum(s for f, s in files.items() if f.startswith(d)) for d in dirs) if s <= 100000)
+
+
+def part2(inp):
+    files, dirs = inp
+    disk_space = 70000000
+    needed_space = 30000000
+    used_space = sum(files.values())
+    additional_required = needed_space-disk_space+used_space
+    return sorted(s for s in (sum(s for f, s in files.items() if f.startswith(d)) for d in dirs) if s >= additional_required)[0]
+
+
+if __name__ == '__main__':
+    inp = [[tuple(l.split(' ')) for l in c.strip().splitlines()] for c in data.split('$')]
+    inp = get_files_and_dirs(inp)
+    try:
+        submit(part1(inp), part="a")
+        submit(part2(inp), part="b")
+    except AocdError as e:
+        print(e)
