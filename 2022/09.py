a87e1678fcd87fcf25d775270c919220fde428e9
bernikr <berni98k@gmail.com>
2022-12-09 10:26:10 +0100

    refactor: part2 is just an more general case of part1

--- 2022/09.py
+++ 2022/09.py
@@ -3,31 +3,25 @@ from itertools import chain
 from aoc_utils import Vec, sign
 from aocd import data, submit, AocdError
 
-dirs = {"U": Vec(0, 1), "D": Vec(0, -1), "L": Vec(-1, 0), "R": Vec(1, 0)}
 
-
-def part1(inp):
-    head = Vec(0, 0)
-    tail = head
-    tail_pos = {tail}
-    for ins in chain.from_iterable((d for _ in range(i)) for d, i in inp):
-        head += dirs[ins]
-        if abs(head - tail) >= 2:
-            tail += Vec(*map(sign, head - tail))
-            tail_pos.add(tail)
-    return len(tail_pos)
-
-
-def part2(inp):
-    rope = [Vec(0, 0) for _ in range(10)]
+def move_rope(inp, rope_length):
+    rope = [Vec(0, 0) for _ in range(rope_length)]
     tail_pos = {rope[-1]}
     for ins in chain.from_iterable((d for _ in range(i)) for d, i in inp):
-        rope[0] += dirs[ins]
-        for i in range(1, 10):
+        rope[0] += {"U": Vec(0, 1), "D": Vec(0, -1), "L": Vec(-1, 0), "R": Vec(1, 0)}[ins]
+        for i in range(1, rope_length):
             if abs(rope[i - 1] - rope[i]) >= 2:
                 rope[i] += Vec(*map(sign, rope[i - 1] - rope[i]))
         tail_pos.add(rope[-1])
     return len(tail_pos)
+
+
+def part1(inp):
+    return move_rope(inp, 2)
+
+
+def part2(inp):
+    return move_rope(inp, 10)
 
 
 if __name__ == '__main__':
