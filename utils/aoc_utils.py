4a2ac17ca63756c591a2ae1166bee83e67a8f26f
bernikr <berni98k@gmail.com>
2022-12-28 21:28:37 +0100

    add I and Y to ocr

--- utils/aoc_utils.py
+++ utils/aoc_utils.py
@@ -88,8 +88,8 @@ def ocr(m: set[tuple[int, int]]):
         v = int("".join('1' if (i + x, y) in m else '0' for y in range(6) for x in range(4)), 2)
         try:
             s += {6922137: "A", 15329694: "B", 6916246: "C", 16312463: "E", 16312456: 'F', 6917015: "G", 10090905: "H",
-                  3215766: "J", 10144425: "K", 8947855: "L", 6920598: "O", 15310472: "P", 15310505: "R", 7898654: "S",
-                  10066326: "U", 15803535: "Z"}[v]
+                  7479847: "I", 3215766: "J", 10144425: "K", 8947855: "L", 6920598: "O", 15310472: "P", 15310505: "R",
+                  7898654: "S", 10066326: "U", 8933922: "Y", 15803535: "Z"}[v]
         except KeyError:
             letter = '\n'.join("".join('▓' if (i + x, y) in m else ' ' for x in range(4)) for y in range(6))
             print(f"Unknown Letter with id {v}:\n\n{letter}")
