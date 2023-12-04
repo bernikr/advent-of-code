ddd4761b393de804270a08b7d1d7f9547ea53ace
bernikr <berni98k@gmail.com>
2023-12-04 11:31:57 +0100

    refactor 2023 04

--- 2023/04.py
+++ 2023/04.py
@@ -1,5 +1,5 @@
 def solve(inp, part1):
-    won = [len(a & b) for a, b in (tuple(map(lambda x: set(x.split()), l[10:].split('|'))) for l in inp.splitlines())]
+    won = [len(a & b) for a, b in (map(lambda x: set(x.split()), l[10:].split('|')) for l in inp.splitlines())]
     if part1:
         return sum(int(2 ** (i - 1)) for i in won)
     else:
