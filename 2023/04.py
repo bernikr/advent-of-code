e7d66b4009efcdb62648e8c11a040a0b0914f2d9
bernikr <berni98k@gmail.com>
2023-12-04 10:57:48 +0100

    refactor 2023 04

--- 2023/04.py
+++ 2023/04.py
@@ -1,13 +1,12 @@
 def solve(inp, part1):
-    won = [len(set(a).intersection(b)) for a, b in
-           (tuple(map(lambda a: [int(x) for x in a.split(' ') if x], l[10:].split('|'))) for l in inp.splitlines())]
+    won = [len(a & b) for a, b in (tuple(map(lambda x: set(x.split()), l[10:].split('|'))) for l in inp.splitlines())]
     if part1:
         return sum(int(2 ** (i - 1)) for i in won)
     else:
         cards = [1] * len(won)
         for i in range(len(won)):
             for j in range(won[i]):
-                cards[i+j+1] += cards[i]
+                cards[i + j + 1] += cards[i]
         return sum(cards)
 
 
