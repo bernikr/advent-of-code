75eb607e5623dd146ee127537d11088883da40ff
bernikr <berni98k@gmail.com>
2023-12-07 15:20:42 +0100

    refactor 2023 07

--- 2023/07.py
+++ 2023/07.py
@@ -1,49 +1,18 @@
 from collections import Counter
-from functools import cmp_to_key
 
+cards = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
 
-def get_hand_type(hand, joker=False):
-    c = Counter(hand)
-    if joker:
-        joker_num = c['J']
-        del c['J']
-        c = sorted(c.values(), reverse=True)
-        if c:
-            c[0] += joker_num
-        else:
-            c = [5]
-    else:
-        c = sorted(c.values(), reverse=True)
-    types = [
-        [1, 1, 1, 1, 1],
-        [2, 1, 1, 1],
-        [2, 2, 1],
-        [3, 1, 1],
-        [3, 2],
-        [4, 1],
-        [5]
-    ]
-    return types.index(c)
+hand_type_1 = lambda x: sorted(Counter(x).values(), reverse=True)
+hand_type_2 = lambda x: max(hand_type_1(x.replace('J', a)) for a in cards)
 
-
-def tie_break_value(hand, joker=False):
-    if joker:
-        cards = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A']
-    else:
-        cards = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
-    return sum(cards.index(c) * len(cards) ** p for p, c in enumerate(reversed(hand)))
-
-
-def compare_hands(a, b, joker=False):
-    type_a, type_b = get_hand_type(a, joker), get_hand_type(b, joker)
-    if type_a - type_b != 0:
-        return type_a - type_b
-    return tie_break_value(a, joker) - tie_break_value(b, joker)
+tie_break_1 = lambda x: [cards.index(c) for c in x]
+tie_break_2 = lambda x: [(['J'] + cards).index(c) for c in x]
 
 
 def solve(inp, part1):
     inp = [(card, int(bid)) for card, bid in (l.split() for l in inp.splitlines())]
-    sorted_hands = sorted(inp, key=cmp_to_key(lambda a, b: compare_hands(a[0], b[0], joker=not part1)))
+    hand_type, tie_break = (hand_type_1, tie_break_1) if part1 else (hand_type_2, tie_break_2)
+    sorted_hands = sorted(inp, key=lambda x: hand_type(x[0]) + tie_break(x[0]))
     return sum(bid * rank for rank, (_, bid) in enumerate(sorted_hands, 1))
 
 
