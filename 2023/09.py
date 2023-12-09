955291f3e88bdae59a8b2cb9acc171ab9d6ec11f
bernikr <berni98k@gmail.com>
2023-12-09 13:28:47 +0100

    refactor 2023 09

--- 2023/09.py
+++ 2023/09.py
@@ -2,15 +2,13 @@ from itertools import pairwise
 
 
 def predict_next(history):
-    if all(x == 0 for x in history):
+    if not any(history):
         return 0
     return history[-1] + predict_next([b - a for a, b in pairwise(history)])
 
 
 def solve(inp, part1):
-    inp = [list(map(int, l.split())) for l in inp.splitlines()]
-    dir = 1 if part1 else -1
-    return sum(predict_next(x[::dir]) for x in inp)
+    return sum(predict_next(x[::1 if part1 else -1]) for x in (list(map(int, l.split())) for l in inp.splitlines()))
 
 
 if __name__ == '__main__':
