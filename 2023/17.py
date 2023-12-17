7ab1b5f7adb04e3f78e19ff2986160df13eecb8f
bernikr <berni98k@gmail.com>
2023-12-17 16:36:10 +0100

    2023 17 smaller search graph
    - remove step counter from state
    - add all straight line steps as neighbors

--- 2023/17.py
+++ 2023/17.py
@@ -1,31 +1,33 @@
 from aoc_utils import a_star
 
 
-def get_neighbor_function(xmax, ymax, step_min, step_max):
+def get_neighbor_function(mapp, xmax, ymax, step_min, step_max):
     def n(state):
-        x, y, dx, dy, moved = state
-        if moved < step_max:
-            if 0 <= x + dx <= xmax and 0 <= y + dy <= ymax:
-                yield x + dx, y + dy, dx, dy, moved + 1
-        if moved >= step_min:
-            if 0 <= x + dy <= xmax and 0 <= y - dx <= ymax:
-                yield x + dy, y - dx, dy, -dx, 1
-            if 0 <= x - dy <= xmax and 0 <= y + dx <= ymax:
-                yield x - dy, y + dx, -dy, dx, 1
+        x, y, dx, dy = state
+        sum_a, sum_b = 0, 0
+        for i in range(1, step_max + 1):
+            if 0 <= x + dy * i <= xmax and 0 <= y - dx * i <= ymax:
+                sum_a += mapp[y - dx * i][x + dy * i]
+                if i >= step_min:
+                    yield (x + dy * i, y - dx * i, dy, -dx), sum_a
+            if 0 <= x - dy * i <= xmax and 0 <= y + dx * i <= ymax:
+                sum_b += mapp[y + dx * i][x - dy * i]
+                if i >= step_min:
+                    yield (x - dy * i, y + dx * i, -dy, dx), sum_b
 
     return n
 
 
 def solve(inp, part1):
-    step_min, step_max = (0, 3) if part1 else (4, 10)
+    step_min, step_max = (1, 3) if part1 else (4, 10)
     mapp = [[int(x) for x in l] for l in inp.splitlines()]
-    starts = {(0, 0, 1, 0, 0), (0, 0, 0, 1, 0)}  # x, y, dx, dy, moves
+    starts = {(0, 0, 1, 0), (0, 0, 0, 1)}  # x, y, dx, dy
     goal = (len(mapp[0]) - 1, len(mapp) - 1)
-    is_goal = lambda s: s[0] == goal[0] and s[1] == goal[1] and s[4] >= step_min
-    neighbors = get_neighbor_function(*goal, step_min, step_max)
-    d = lambda c, n: mapp[n[1]][n[0]]
+    is_goal = lambda s: s[0] == goal[0] and s[1] == goal[1]
+    neighbors = get_neighbor_function(mapp, *goal, step_min, step_max)
     h = lambda s: goal[0] - s[0] + goal[1] - s[1]
-    return a_star(starts, is_goal, neighbors, d, h)[1]
+    path, cost = a_star(starts, is_goal, neighbors, h=h)
+    return cost
 
 
 if __name__ == '__main__':
