54d53180611db4d2d168dbb678741a35905cbccb
bernikr <berni98k@gmail.com>
2023-12-20 15:42:27 +0100

    refactor 2023 20

--- 2023/20.py
+++ 2023/20.py
@@ -7,9 +7,19 @@ HIGH, LOW = True, False
 ON, OFF = True, False
 
 
-def run(node_type, node_connections, flip_state, conj_state, inital_pulse):
+def get_initial_state(graph):
+    node_type, node_connections = graph
+    flip_state = {name: OFF for name, t in node_type.items() if t == "%"}
+    conj_state = {name: {n: LOW for n, conns in node_connections.items() if name in conns}
+                  for name, t in node_type.items() if t == "&"}
+    return flip_state, conj_state
+
+
+def run(graph, state):
+    node_type, node_connections = graph
+    flip_state, conj_state = state
     msg_queue = deque()
-    msg_queue.append(inital_pulse)
+    msg_queue.append(("button", "roadcaster", LOW))
     while msg_queue:
         from_module, to_module, pulse = msg_queue.popleft()
         yield from_module, to_module, pulse
@@ -33,34 +43,33 @@ def run(node_type, node_connections, flip_state, conj_state, inital_pulse):
             assert False
 
 
-def solve_part1(node_type, node_connections):
-    flip_state = {name: OFF for name, t in node_type.items() if t == "%"}
-    conj_state = {name: {n: LOW for n, conns in node_connections.items() if name in conns}
-                  for name, t in node_type.items() if t == "&"}
+def solve_part1(graph):
+    state = get_initial_state(graph)
     pulses = [0, 0]
     for _ in range(1000):
-        for _, _, p in run(node_type, node_connections, flip_state, conj_state, ("button", "roadcaster", LOW)):
+        for _, _, p in run(graph, state):
             pulses[p] += 1
 
     return pulses[0] * pulses[1]
 
 
-def solve_part2(node_type, node_connections):
-    # assume a connection graph that consists of x subgraphs connected at the broadcaster and one & node before rx
+def solve_part2(graph):
+    node_type, node_connections = graph
+    # assume that there is exactly one & node before rx whose inputs all have a simple periodicity
+    # simple = turn on every x button pushes
     combiner_node = [n for n, conns in node_connections.items() if "rx" in conns]
     assert len(combiner_node) == 1 and node_type[combiner_node[0]] == "&"
     combiner_node = combiner_node[0]
 
-    flip_state = {name: OFF for name, t in node_type.items() if t == "%"}
-    conj_state = {name: {n: LOW for n, conns in node_connections.items() if name in conns}
-                  for name, t in node_type.items() if t == "&"}
-    seen = {n: [] for n, conns in node_connections.items() if combiner_node in conns}
+    state = get_initial_state(graph)
+    seen = {n: 0 for n, conns in node_connections.items() if combiner_node in conns}
     for i in count(1):
-        for from_m, to_m, p in run(node_type, node_connections, flip_state, conj_state, ("button", "roadcaster", LOW)):
+        for from_m, to_m, p in run(graph, state):
             if p == HIGH and to_m == combiner_node:
-                seen[from_m].append(i)
-                if all(len(x) >= 1 for x in seen.values()):
-                    return reduce(math.lcm, (x[0] for x in seen.values()))
+                if not seen[from_m]:
+                    seen[from_m] = i
+                if all(x for x in seen.values()):
+                    return reduce(math.lcm, seen.values())
 
 
 def solve(inp, part1):
@@ -71,10 +80,11 @@ def solve(inp, part1):
         ntype, name = name[0], name[1:]
         node_type[name] = ntype
         node_connections[name] = conns.split(", ")
+    graph = node_type, node_connections
     if part1:
-        return solve_part1(node_type, node_connections)
+        return solve_part1(graph)
     else:
-        return solve_part2(node_type, node_connections)
+        return solve_part2(graph)
 
 
 if __name__ == '__main__':
