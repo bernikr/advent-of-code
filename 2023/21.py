f2c5ce22febb4a0247e35ad3022f7f852a063c93
bernikr <berni98k@gmail.com>
2023-12-21 13:57:43 +0100

    2023 21 bugfix float64 is not precise enough for some results

--- 2023/21.py
+++ 2023/21.py
@@ -23,20 +23,20 @@ def solve(inp, part1):
         n = xmax + 1
         steps = 26501365
         a, r = steps // n, steps % n  # repeats and remainder
-        interesting_steps = [r, n + r, 2 * n + r]
 
         seen = set()
         front = {start}
         count = [0, 0]
-        counts = [0] * (interesting_steps[-1] + 1)
+        counts = [0] * (n * 2 + r + 1)
         for i in range(n * 2 + r + 1):
             count[i % 2] += len(front)
             counts[i] = count[i % 2]
             seen |= front
             front = {(p + d) for p in front for d in dirs4 if (p + d).pos_mod(n) not in walls and p + d not in seen}
 
-        poly = np.polyfit(interesting_steps, [counts[s] for s in interesting_steps], 2)
-        return round(np.polyval(poly, steps))
+        poly = np.polyfit([0, 1, 2], [counts[a * n + r] for a in [0, 1, 2]], 2)
+        x = (steps - r) // n
+        return round(poly[0]) * x * x + round(poly[1]) * x + round(poly[2])
 
 
 if __name__ == '__main__':
