872631e007a77e5a0416060dba96441f4b8b8b53
bernikr <berni98k@gmail.com>
2023-12-22 11:46:17 +0100

    2023 22 slow but working

---
+++ 2023/22.py
@@ -0,0 +1,75 @@
+import math
+import re
+from collections import defaultdict
+from functools import cache
+
+from aoc_utils import Box, Vec
+
+DOWN = Vec(0, 0, -1)
+
+
+@cache
+def settle_bricks(inp):
+    falling = sorted([Box((x1, y1, z1), (x2, y2, z2)) for x1, y1, z1, x2, y2, z2 in
+                      (tuple(map(int, re.findall(r"\d+", l))) for l in inp.splitlines())], key=lambda b: b.lower[2])
+    ground = Box((0, 0, 0), (math.inf, math.inf, 0))
+    settled = defaultdict(list)
+    settled[0].append((0, ground))
+    settled_next_i = 1
+    supported_by = defaultdict(list)
+    while falling:
+        settled_changed = False
+        new_falling = []
+        for b in falling:
+            b.lower += DOWN
+            b.upper += DOWN
+            for i, other in settled[b.lower[2]]:
+                if b.overlaps(other):
+                    supported_by[settled_next_i].append(i)
+            b.lower -= DOWN
+            b.upper -= DOWN
+            if supported_by[settled_next_i]:
+                settled[b.upper[2]].append((settled_next_i, b))
+                settled_next_i += 1
+                settled_changed = True
+            else:
+                new_falling.append(b)
+        falling = new_falling
+        if not settled_changed:
+            for b in falling:
+                b.lower += DOWN
+                b.upper += DOWN
+    return supported_by
+
+
+def calculate_fallen(supported_by, disintegrate):
+    supports = {a: list(b) for a, b in supported_by.items()}
+    fallen = 0
+    to_destroy = {disintegrate}
+    while to_destroy:
+        b = to_destroy.pop()
+        for i, l in supports.items():
+            if b in l:
+                l.remove(b)
+                if not l:
+                    fallen += 1
+                    to_destroy.add(i)
+    return fallen
+
+
+def solve(inp, part1):
+    supported_by = settle_bricks(inp)
+    if part1:
+        return sum(calculate_fallen(supported_by, i) == 0 for i in range(1, len(supported_by) + 1))
+    else:
+        return sum(calculate_fallen(supported_by, i) for i in range(1, len(supported_by) + 1))
+
+
+if __name__ == '__main__':
+    from aocd import data, submit, AocdError
+
+    try:
+        submit(solve(data, True), part="a")
+        submit(solve(data, False), part="b")
+    except AocdError as e:
+        print(e)
