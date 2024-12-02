f43377950048c11bf22ddd925b4f747f2ce1b0b1
bernikr <git@berni.kr>
2024-12-02 09:25:26 +0100

    2024 02

---
+++ 2024/02.py
@@ -0,0 +1,24 @@
+from itertools import pairwise
+from typing import Iterable
+
+
+def is_safe(seq: Iterable[int]) -> bool:
+    diff = [a - b for a, b in pairwise(seq)]
+    return ((all(d > 0 for d in diff) or all(d < 0 for d in diff))
+            and all(1 <= abs(d) <= 3 for d in diff))
+
+
+def solve(inp: str) -> str | int:
+    inp = [tuple(map(int, l.split())) for l in inp.splitlines()]
+    yield 1, sum(is_safe(l) for l in inp)
+    yield 2, sum(any(is_safe(l[:i] + l[i + 1:]) for i in range(len(l) + 1)) for l in inp)
+
+
+if __name__ == "__main__":
+    from aocd import data, submit, AocdError
+
+    try:
+        for part, solution in solve(data):
+            submit(solution, part=("a", "b")[part - 1])
+    except AocdError as e:
+        print(e)
