cfbed1c324be46aa3cd561eaaf79877ddbd3a38d
bernikr <git@berni.kr>
2024-12-03 11:47:33 +0100

    2024 03

---
+++ 2024/03.py
@@ -0,0 +1,17 @@
+import re
+
+
+def solve(inp: str) -> str | int:
+    part1 = lambda x: sum(int(a) * int(b) for a, b in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", x))
+    yield 1, part1(inp)
+    yield 2, part1(" ".join(a.split("don't()")[0] for a in inp.split("do()")))
+
+
+if __name__ == "__main__":
+    from aocd import data, submit, AocdError
+
+    try:
+        for part, solution in solve(data):
+            submit(solution, part=("a", "b")[part - 1])
+    except AocdError as e:
+        print(e)
