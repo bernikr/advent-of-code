9c7e9bf0f480fc984d997905901b27795b1bbdbf
bernikr <git@berni.kr>
2024-12-05 13:55:51 +0100

    lint entrypoint.py

--- aoc_runner/entrypoint.py
+++ aoc_runner/entrypoint.py
@@ -2,17 +2,19 @@ import importlib
 import inspect
 
 
-def solve(year, day, data):
+def solve(year: int, day: int, data: str) -> tuple:
     mod_name = f"{year}.{day:02}"
     mod = importlib.import_module(mod_name)
 
     if len(inspect.signature(mod.solve).parameters) == 2:
-        a = mod.solve(data, True)
-        b = mod.solve(data, False)
+        a = mod.solve(data, True)  # noqa: FBT003
+        b = mod.solve(data, False)  # noqa: FBT003
         return a, b
-    else:
+    if len(inspect.signature(mod.solve).parameters) == 1:
         sol = [None, None]
         for part, solution in mod.solve(data):
             sol[part - 1] = solution
             if all(sol):
                 return tuple(sol)
+
+    raise NotImplementedError
