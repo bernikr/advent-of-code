22ceb0cb92616ac0051cfce879c50a441e93d64c
bernikr <git@berni.kr>
2025-12-01 07:28:40 +0100

    2025 01

---
+++ 2025/01.py
@@ -0,0 +1,19 @@
+from collections.abc import Iterable
+from itertools import accumulate, chain
+
+
+def solve(inp: str) -> Iterable[tuple[int, int | str]]:
+    inp: list[int] = [int(l[1:]) * (-1 if l[0] == "L" else 1) for l in inp.splitlines()]
+    yield 1, sum(1 if x % 100 == 0 else 0 for x in accumulate(inp, initial=50))
+    inp = list(chain.from_iterable([x // abs(x)] * abs(x) for x in inp))
+    yield 2, sum(1 if x % 100 == 0 else 0 for x in accumulate(inp, initial=50))
+
+
+if __name__ == "__main__":
+    from aocd import AocdError, data, submit
+
+    try:
+        for part, solution in solve(data):
+            submit(solution, part=("a", "b")[part - 1])
+    except AocdError as e:
+        print(e)
