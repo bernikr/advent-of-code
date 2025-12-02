ba7e62962641c6954c66a7b710056a6acc72da50
bernikr <git@berni.kr>
2025-12-02 08:30:50 +0100

    2025 02

---
+++ 2025/02.py
@@ -0,0 +1,34 @@
+from collections.abc import Iterable
+from typing import cast
+
+
+def is_valid(x: int) -> bool:
+    x = str(x)
+    return x[: len(x) // 2] != x[len(x) // 2 :]
+
+
+def is_valid2(x: int) -> bool:
+    x = str(x)
+    l = len(x)
+    for i in range(1, l // 2 + 1):
+        if l % i != 0:
+            continue
+        if all(x[:i] == x[i * j : i * (j + 1)] for j in range(1, l // i)):
+            return False
+    return True
+
+
+def solve(inp: str) -> Iterable[tuple[int, int | str]]:
+    inp: list[tuple[int, int]] = [cast("tuple[int, int]", tuple(map(int, a.split("-")))) for a in inp.split(",")]
+    yield 1, sum(i for a, b in inp for i in range(a, b + 1) if not is_valid(i))
+    yield 2, sum(i for a, b in inp for i in range(a, b + 1) if not is_valid2(i))
+
+
+if __name__ == "__main__":
+    from aocd import AocdError, data, submit
+
+    try:
+        for part, solution in solve(data):
+            submit(solution, part=("a", "b")[part - 1])
+    except AocdError as e:
+        print(e)
