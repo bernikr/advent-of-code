30c2b94f2d591dd89f0eb13b4acffe9565b44b01
bernikr <git@berni.kr>
2025-12-03 09:24:05 +0100

    optimize 2025 02

--- 2025/02.py
+++ 2025/02.py
@@ -1,28 +1,31 @@
 from collections.abc import Iterable
+from itertools import chain, count
+
+from more_itertools import unique
 
 from aoc_utils import tuple2
 
 
-def is_valid(x: int) -> bool:
-    x = str(x)
-    return x[: len(x) // 2] != x[len(x) // 2 :]
-
-
-def is_valid2(x: int) -> bool:
-    x = str(x)
-    l = len(x)
-    for i in range(1, l // 2 + 1):
-        if l % i != 0:
+def generate_invalid(minn: int, maxx: int, reps: int) -> Iterable[int]:
+    min_str = str(minn)
+    start = int(min_str[: len(min_str) // reps]) if len(min_str) >= reps else 0
+    for i in count(start):
+        x = int(str(i) * reps)
+        if x < minn:
             continue
-        if all(x[:i] == x[i * j : i * (j + 1)] for j in range(1, l // i)):
-            return False
-    return True
+        if x > maxx:
+            break
+        yield x
+
+
+def generate_all_invalid(minn: int, maxx: int) -> Iterable[int]:
+    return unique(chain.from_iterable(generate_invalid(minn, maxx, i) for i in range(2, len(str(maxx)) + 1)))
 
 
 def solve(inp: str) -> Iterable[tuple[int, int | str]]:
     inp: list[tuple[int, int]] = [tuple2(map(int, a.split("-"))) for a in inp.split(",")]
-    yield 1, sum(i for a, b in inp for i in range(a, b + 1) if not is_valid(i))
-    yield 2, sum(i for a, b in inp for i in range(a, b + 1) if not is_valid2(i))
+    yield 1, sum(i for a, b in inp for i in generate_invalid(a, b, 2))
+    yield 2, sum(i for a, b in inp for i in generate_all_invalid(a, b))
 
 
 if __name__ == "__main__":
