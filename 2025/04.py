41afab4b5d4e351a264b9d624bc487620f0ec5d0
bernikr <git@berni.kr>
2025-12-04 10:17:21 +0100

    2025 04 optimizations

--- 2025/04.py
+++ 2025/04.py
@@ -5,16 +5,18 @@ from aoc_utils import create_map, dirs8
 
 
 def solve(inp: str) -> Iterable[tuple[int, int | str]]:
-    mapp = {p for p, c in create_map(inp).items() if c == "@"}
+    rolls = {p for p, c in create_map(inp).items() if c == "@"}
+    lookahead = rolls
     removed = 0
     for i in count():
-        removable = {p for p in mapp if sum(1 for d in dirs8 if p + d in mapp) < 4}
+        removable = {p for p in lookahead if sum(1 for d in dirs8 if p + d in rolls) < 4}
         if i == 0:
             yield 1, len(removable)
         if not removable:
             yield 2, removed
             return
-        mapp -= removable
+        rolls -= removable
+        lookahead = {p + d for p in removable for d in dirs8} & rolls
         removed += len(removable)
 
 
