d37a3251c69603336b583386b97f2770c7c8b25f
bernikr <git@berni.kr>
2025-12-06 09:15:03 +0100

    2025 06 part 2

--- 2025/06.py
+++ 2025/06.py
@@ -7,8 +7,24 @@ def solve(inp: str) -> Iterable[tuple[int, int | str]]:
     inp1 = [l.split() for l in inp.splitlines()]
     numbers: list[list[int]] = [list(map(int, l)) for l in inp1[:-1]]
     operators: list[str] = inp1[-1]
-    yield 1, sum(reduce(operator.add if op == "+" else operator.mul, nums) for *nums, op in zip(*numbers, operators))
-    yield 2, ""
+    yield (
+        1,
+        sum(
+            reduce(operator.add if op == "+" else operator.mul, nums)
+            for *nums, op in zip(*numbers, operators, strict=True)
+        ),
+    )
+    new_inp = [
+        list(map(int, p.splitlines()))
+        for p in "\n".join("".join(map(str.strip, x)) for x in zip(*inp.splitlines()[:-1])).split("\n\n")
+    ]
+    yield (
+        2,
+        sum(
+            reduce(operator.add if op == "+" else operator.mul, nums)
+            for nums, op in zip(new_inp, operators, strict=True)
+        ),
+    )
 
 
 if __name__ == "__main__":
