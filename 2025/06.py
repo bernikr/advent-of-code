3b533b4d80e01738b28067a7a70398b45af660e0
bernikr <git@berni.kr>
2025-12-06 09:23:08 +0100

    2025 06 refactor

--- 2025/06.py
+++ 2025/06.py
@@ -4,27 +4,20 @@ from functools import reduce
 
 
 def solve(inp: str) -> Iterable[tuple[int, int | str]]:
-    inp1 = [l.split() for l in inp.splitlines()]
-    numbers: list[tuple[int, ...]] = list(zip(*(list(map(int, l)) for l in inp1[:-1])))
-    operators: list[str] = inp1[-1]
-    yield (
-        1,
-        sum(
-            reduce(operator.add if op == "+" else operator.mul, nums)
-            for nums, op in zip(numbers, operators, strict=True)
-        ),
-    )
-    new_inp = [
-        list(map(int, p.splitlines()))
+    operators: list[str] = inp.splitlines()[-1].split()
+    numbers1: list[tuple[int, ...]] = list(zip(*(map(int, l.split()) for l in inp.splitlines()[:-1])))
+    numbers2: list[tuple[int, ...]] = [
+        tuple(map(int, p.splitlines()))
         for p in "\n".join("".join(map(str.strip, x)) for x in zip(*inp.splitlines()[:-1])).split("\n\n")
     ]
-    yield (
-        2,
-        sum(
-            reduce(operator.add if op == "+" else operator.mul, nums)
-            for nums, op in zip(new_inp, operators, strict=True)
-        ),
-    )
+    for i, numbers in [(1, numbers1), (2, numbers2)]:
+        yield (
+            i,
+            sum(
+                reduce(operator.add if op == "+" else operator.mul, nums)
+                for nums, op in zip(numbers, operators, strict=True)
+            ),
+        )
 
 
 if __name__ == "__main__":
